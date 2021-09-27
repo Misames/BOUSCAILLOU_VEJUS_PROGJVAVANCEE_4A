489d00fa84f85c00c40daa6c2b954e90a5d88fe4
François Bouscaillou <fbouscaillou@myges.fr>
2021-09-27 20:47:32 +0200

    writting game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int gameDuration = 60;
+    GameObject firstPlayer;
+    GameObject secondPlayer;
+    GameObject PauseMenu;
+
+    void Update()
+    {
+        // check condition de victoire
+        // Pause
+        // Exit menu
+    }
+
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int life = 100;
+
+    void Punch(GameObject enemie)
+    {
+
+    }
+
+    void Kick(GameObject enemie)
+    {
+
+    }
+
+    void Block()
+    {
+
+    }
+}
