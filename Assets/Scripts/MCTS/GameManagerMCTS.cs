acd276e485acefd684c9fafad1502d20d2b65a3a
François Bouscaillou <fbouscaillou@myges.fr>
2021-09-29 14:05:24 +0200

    start MCTS

---
+++ Assets/Scripts/MCTS/GameManagerMCTS.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerMCTS : MonoBehaviour
+{
+    
+    PlayerMCTS[] mesPlayer;
+    float time, timeDuration;
+
+    int EndGame(int code)
+    {
+        return code;
+    }
+}
